3ebdc31ddf6458b1ce9d1fd20f5fc568740774a1
SheikhNegarSultana <sultanashashi64@gmail.com>
2024-11-04 16:30:58 +0600

    SetUp Done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1 className=" text-teal-400">Vite + React</h1>
+      
+      <button className=" btn btn-primary"></button>
+    </>
+  )
+}
+
+export default App
